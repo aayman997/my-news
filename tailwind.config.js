bb3d7700a6eb1fcfc430a64cda6df063fc93b254
Ahmed Ayman <ahmed1997164@gmail.com>
2023-09-05 00:58:17 +0300

    adding prettier and tailwaind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import("tailwindcss").Config} */
+export default {
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		fontFamily: {
+			sans: "Ubuntu, sans-serif",
+		},
+		extend: {
+			height: {
+				screen: "100dvh",
+			},
+		},
+	},
+	plugins: [],
+};
